2e389fd304e03b13c405aeba07d23adac5467e66
filipodu982 <filipodu982@gmail.com>
2017-07-12 11:26:24 +0200

    First way to TSP
    Testing all possible permutations of a given array. Not the most effective way because there are n! possible solutions, where n is the number of cities. In future, I'm going to improve this way, because right now I'm using python library called random, which randomly shuffles the order of cities in an array. However, it often repeats itself because there is no function checking whether the array has been tested so far.

---
+++ TSP_bruteForce.py
@@ -0,0 +1,59 @@
+import pygame, random, math,time
+#some setup and variables/lists
+pygame.init()
+passed = 0                                                                      #how many cycles has already passed
+size = (400,400)
+screen = pygame.display.set_mode((size))
+done = False
+points = 20                                                                      #how many cities do we want?
+cords = []                                                                      #array storing coordinates of every city e.g. [(255,350), (310,270)]
+black = 0, 0, 0                                                                 #RGB values of black color
+best = 999999999999999
+pop = []                                                                        #the order in which the points should be visited
+
+for i in range(points):                                                         #generating first order to begin with
+    pop.append(i)
+
+clock = pygame.time.Clock()
+howMany = math.factorial(points)                                                #how many permutations there are
+
+#function calculating distance betweent points in array
+def calcDistance(array):
+    dist = 0
+    for i in range(len(array)-1):
+        dist += math.hypot((cords[array[i]][0] - cords[array[i+1]][0]), (cords[array[i]][1] - cords[array[i+1]][1]))
+    return dist
+
+#generating coordinates of circles
+for i in range(points):
+    x = random.randint(0,size[0])
+    y = random.randint(0,size[0])
+    #pygame.draw.circle(screen, (255,255,255), (x, y),5)
+    cords.append((x,y))
+    #pygame.display.update()
+
+#main loop
+while not done:
+    d = calcDistance(pop)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    if d < best:
+        screen.fill(black)
+        #drawing circles all over again in each frame. Not the most effective solution, but it's the only one so far
+        for i in cords:
+            pygame.draw.circle(screen, (255,255,255), i,5)
+            pygame.display.update()
+
+        #drawing the best route
+        for i in range(len(pop) - 1):
+            pygame.draw.line(screen, (255,255,255), cords[pop[i]], cords[pop[i+1]])
+            pygame.display.update()
+            best = d
+
+    random.shuffle(pop)                                                         #shuffling the order of cities
+    passed += 1.0
+    print round((passed/howMany)*100,2)                                                  #percent of finished permutations. however not the most accurate way, because shuffle function repeats some permutations which are not checked anywhere
+    clock.tick(60)                                                              #how many FPS
